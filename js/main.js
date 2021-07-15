0895f5eaaa46bc024dc390ff4b862dbc1098a012
l22hye0n500 <mayring826@gmail.com>
2021-07-15 16:34:35 +0900

    공통 모듈 분리

--- js/main.js
+++ js/main.js
@@ -1,20 +1,4 @@
-const searchEl = document.querySelector('.search');
-const searchInputEl = searchEl.querySelector('input');
 
-searchEl.addEventListener('click', function(){
-  searchInputEl.focus();
-});
-
-
-searchInputEl.addEventListener('focus', function(){
-  searchEl.classList.add('focused');
-  searchInputEl.setAttribute('placeholder','통합검색');
-});
-
-searchInputEl.addEventListener('blur', function(){
-  searchEl.classList.remove('focused');
-  searchInputEl.setAttribute('placeholder','');
-});
 
 
 const badgeEL = document.querySelector('header .badges');
@@ -185,12 +169,3 @@ spyEls.forEach(function(spyEl){
     .addTo(new ScrollMagic.Controller());
 
 });
-
-
-
-// 년도 자동계산
-
-const thisYear = document.querySelector('.this-year');
-thisYear.textContent = new Date().getFullYear(); //2021
-
-
